1d09d025d806016ff9c43c73831f297dc4b0b521
Steve Ruben <sruben@validas.com>
2012-08-16 12:44:53 -0500

    Updated map functions to include document type.

--- CouchbaseDemo/MapReduce.js
+++ CouchbaseDemo/MapReduce.js
@@ -1,12 +1,16 @@
 ﻿//GetUsersByPurchaseCountRange
 function (doc) {
-    emit(doc.PurchaseCount, null);
+    if (doc.Type == "User") {
+        emit(doc.PurchaseCount, null);
+    }
 }
 
 //GetUsersByRole
 function (doc) {
-    for (i = 0; i < doc.Roles.length; i++) {
-        var role = doc.Roles[i];
-        emit(role.RoleName, doc);
+    if (doc.Type == "User") {
+        for (i = 0; i < doc.Roles.length; i++) {
+            var role = doc.Roles[i];
+            emit(role.RoleName, doc);
+        }
     }
 }
